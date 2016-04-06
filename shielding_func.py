ac58e395e58ffd2b8dd3940caa7389bd4dc16676
wronk <wronk.mark@gmail.com>
2016-04-06 13:50:48 -0700

    Add vector angle differences, split up power calc

--- shielding_func.py
+++ shielding_func.py
@@ -55,14 +55,39 @@ def comp_shielding(raw, raw_sss):
     return raw_power / sss_power
 
 
-def get_power(raw):
+def get_power(raw, picks=None):
     """Helper to compute power of magnetometer channels in raw object"""
 
     # Grab only magnetometers for calculation (though both mags and grads
     # affect shielding factor calculation)
-    picks = pick_types(raw.info, meg='mag')
-    assert np.all(picks == pick_types(raw.info, meg='mag')), \
-        'Channel mismatch'
+    if picks is None:
+        picks = pick_types(raw.info, meg='mag')
 
     # Return norm along channel axis
-    return norm(raw[picks][0], axis=0)
+    return norm(raw[picks, :][0], axis=0)
+
+
+def unit_vector(vector):
+    """ Returns the unit vector of the vector.  """
+    return vector / np.linalg.norm(vector)
+
+
+def angle_between(v1, v2, metric='radian'):
+    """ Returns the angle in radians between vectors 'v1' and 'v2'::
+
+            >>> angle_between((1, 0, 0), (0, 1, 0))
+            1.5707963267948966
+            >>> angle_between((1, 0, 0), (1, 0, 0))
+            0.0
+            >>> angle_between((1, 0, 0), (-1, 0, 0))
+            3.141592653589793
+    """
+    v1_u = unit_vector(v1)
+    v2_u = unit_vector(v2)
+
+    if metric == 'radian':
+        return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
+    elif metric == 'degree':
+        return np.rad2deg(np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0)))
+    else:
+        raise RuntimeError('Input metric not recognized')
